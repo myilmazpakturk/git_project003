18686557d6ff2dcdbb09d4d6ce62500cbdf0588e
myilmazpakturk <67061931+myilmazpakturk@users.noreply.github.com>
2020-07-05 14:04:54 -0400

    Update Class1.java
    from remote master

--- src/git_project003/Class1.java
+++ src/git_project003/Class1.java
@@ -5,5 +5,8 @@ public class Class1 {
 	public static void main(String[] args) {
 		System.out.println("local master branch");
 		System.out.println("local master branch 2. commit");
+		System.out.println("local master branch");
+		System.out.println("local master branch 2. commit");
+		
 	}
 }
